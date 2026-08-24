ff1e9e4d881ddeb67890cca5b847665c142364ad
rezalyt <rezalyt144@gmail.com>
2026-08-24 20:51:47 +0700

    chore: define reliable provider allowlist

---
+++ search-service/provider-policy.js
@@ -0,0 +1,16 @@
+export const SEARCH_PROVIDERS = [
+  'polyhaven',
+  'printables',
+  'thangs',
+  'nasa',
+  'ambientcg',
+  'sketchfab',
+];
+
+export const EXCLUDED_PROVIDERS = [
+  'cgtrader',
+  'cults3d',
+  'free3d',
+  'blendswap',
+  'grabcad',
+];
